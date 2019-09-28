fd7987761f392bcc6225e74ea99f0e808e960df7
Saagmehta97 <44450006+Saagmehta97@users.noreply.github.com>
2019-09-28 05:15:47 -0400

    Legislative Calendar

---
+++ legislative calendar.sql
@@ -0,0 +1,161 @@
+CREATE TABLE Team_8.legislative_calendar
+( 
+state varchar(20),
+convenes varchar(15),
+adjourns varchar(15),
+carryover varchar(5),
+pre_filing_open_or_close varchar(30)
+);
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Alabama','2/4/20','3/27/20', 'N',  'Open- Feb. 3');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Alaska','1/20/20','5/15/20', 'Y',  'Open- Jan. 1');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Arizona','1/13/20','4/30/20', 'N',  'Open- Jan. 12');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Arkansas','4/6/20','5/7/20', 'N',  'No Pre-Filing[Even Year');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('California','12/3/18','11/30/20', 'Y',  'No Pre-Filing');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Colorado','1/8/20','5/8/20', 'N',  'Open Jan. 3');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Connecticut','2/5/20','6/3/20', 'N',  'Open [Fiscal Bills Only]');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Delaware','1/1/20','6/30/20', 'Y',  'Dec 12.-Jan.19');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('District of Columbia','1/2/20','12/31/20', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Florida','1/14/20','3/13/20', 'N',  'Open- Jan. 3');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Georgia','1/12/20','3/27/20', 'Y',  'Nov 15.-Jan. 11');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Hawaii','1/15/20','5/5/20', 'Y',  'Nov. 15-Jan. 11');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Idaho','1/6/20','3/27/20', 'N',  'No Pre-Filling');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Idaho','1/6/20','3/27/20', 'N',  'No Pre-Filling');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Illinois','10/27/20','1/6/21', 'Y',  'Nov. 30-Jan.5');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Indiana','1/3/20','3/14/20', 'N',  'Dec.14-Jan.12(Senate Only)');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Iowa','1/13/20','4/21/20', 'Y',  'Open-Jan.10');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Kansas','1/13/20','5/29/20', 'Y',  'Open-Jan.10');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Kentucky','1/7/20','3/30/20', 'N',  'Open-Jan.6');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Louisiana','3/9/20','5/11/20', 'N',  'Open-Feb.28');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Maine','1/8/20','4/20/20', 'Y',  'Open-Jan. 7');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Maryland','1/8/20','4/10/20', 'N',  'Open-Nov. 30');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Massachusetts','1/1/20','1/6/21', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Michigan','1/9/20','12/31/20', 'Y',  'No Pre-Filing');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Minnesota','2/11/20','5/18/20', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Mississippi','1/7/20','3/27/20', 'N',  'Open -Feb. 10');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Missouri','1/9/20','5/30/20', 'N',  'Dec. 1- Jan. 8');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Montana','Null','Null', 'Null',  'No Even-Year Session');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Nebraska','1/8/20','4/17/20', 'Y',  'No Pre-Filing');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Nevada','Null','Null', 'Null',  'No Even-Year Session');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('New Hampshire','1/8/20','4/17/20', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('New Jersey','1/14/20','1/12/21', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('New Mexico','1/21/20','2/20/20', 'N',  'Dec.16-Jan.17');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('New York','1/7/20','2/1/20', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('North Carolina','1/9/20','7/1/20', 'Y',  'No Pre-Filing');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('North Dakota','Null','Null', 'Null',  'No Even-Year Session');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Oklahoma','2/3/20','5/29/20', 'Y',  'Nov. 15-Jan.16');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Oregon','2/3/20','3/7/20', 'N',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Pennsylvania','2/3/20','TBA', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Rhode Island','1/7/20','6/30/20', 'N',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('South Carolina','1/14/20','6/8/20', 'Y',  'Open[Even Year Only]');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('South Dakota','1/14/20','3/30/20', 'N',  'Dec.15-Jan.13');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Tennessee','1/14/20','4/24/20', 'Y',  'Open[Even Year Only]');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Texas','Null','Null', 'Null',  'No Even-Year Session');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Utah','1/27/20','3/6/20', 'N',  'Open[Even Year Only]');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Vermont','1/7/20','5/12/20[TBA]', 'Y',  'Open');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Virginia','1/8/20','3/8/20', 'Y',  'Nov. 18-Jan. 7');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Washington','1/13/20','3/14/20', 'Y',  'Dec. 16-Jan. 12');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('West Virginia','1/8/20','3/6/20', 'Y',  'Open[House, even-year only]');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Wisconsin','1/14/20','1/4/20', 'Y',  'No Pre-Filing');
+
+INSERT INTO legislative_calendar (state, convenes, adjourns, carryover, pre_filing_open_or_close)
+VALUES ('Wyoming','2/10/20','3/13/20', 'N',  'Open[Fiscal Bills Only]');
